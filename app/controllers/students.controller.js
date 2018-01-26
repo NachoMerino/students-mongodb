5ace0058bb071a78548d59ab59f4f81ed143659f
Nacho Merino <escatman@gmail.com>
2018-01-26 10:43:21 +0100

    all working --css missing in some parts

--- app/controllers/students.controller.js
+++ app/controllers/students.controller.js
@@ -3,43 +3,49 @@ const Student = require('../models/studentmodel');
 exports.findAll = (req, res) => {
   Student.find((err, data) => {
     if (err) {
-      res.status(500).send({ message: 'some error occurred while retrieving students' });
+      res.status(500).send({ error: 'some error occurred while retrieving students' });
     } else {
       res.send(data);
     }
-  })
+  });
 };
 
 exports.findOne = (req, res) => {
-  console.log('finding student:', req.params.studentId);
-  Student.findById(req.params.studentId, (err, student) => {
-    res.send(student);
+  if (!req.params) {
+    return res.send('Field can not be empty');
+  }
+  Student.findById(req.params.studentId, (err, data) => {
+    if (err) {
+      res.status(500).send({ error: 'some error occurred while retrieving the student' });
+    } else {
+      res.send(data);
+    }
   });
 };
 
 exports.create = (req, res) => {
-  if(!req.body){
-    return res.send('some error occurred with req.body');
+  if (!req.body) {
+    return res.send('You send me empty data');
   }
-  console.log('creating new student');
   const newStudent = new Student({
     name: req.body.name,
     age: req.body.age,
     subjects: req.body.subjects,
-    address : { street: req.body.street, postal: req.body.postal, city: req.body.city },
+    address: { street: req.body.street, postal: req.body.postal, city: req.body.city },
     gender: req.body.gender,
   });
   newStudent.save((err) => {
     if (err) {
       throw err;
     }
-    console.log(`user ${newStudent.name} has been saved successfully`);
   });
-  res.send(`user ${newStudent.name} has been saved successfully`);
+  res.send({ message:`Student ${newStudent.name} has been saved successfully`});
 };
 
 exports.update = (req, res) => {
-  console.log('Going to edit', req.params.studentId);
+  if (!req.params) {
+    return res.send('Fields can not be empty');
+  }
   Student.findById(req.params.studentId, (err, student) => {
     student.name = req.body.name;
     student.age = req.body.age;
@@ -50,22 +56,21 @@ exports.update = (req, res) => {
       if (err) {
         throw err;
       }
-      console.log(`student updated`);
     });
   });
   res.json({ message: `student has been update successfully` });
 };
 
 exports.delete = (req, res) => {
-  console.log('deleting new student');
+  if (!req.params) {
+    return res.send('Fields can not be empty');
+  }
   Student.findById(req.params.studentId, (err, student) => {
-    console.log(student);
     student.remove((err) => {
       if (err) {
         throw err;
       }
-      console.log('student deleted with id:', req.params.studentId);
     });
   });
-  res.json({ message: `student has been deleted successfully` });
+  res.json({ message:`student has been deleted successfully` });
 };
--- app/views/assets/js/script.js
+++ app/views/assets/js/script.js
@@ -6,6 +6,7 @@ $().ready(() => {
   // all functions
   function renderSubjects(subjectsArray, button) {
     let subjects = '';
+    // show subjects as a button or no
     if (button) {
       subjectsArray.forEach((subject) => {
         subjects += `<button type = "button" class = "btn btn-outline-info" >${subject}</button>   `;
@@ -51,7 +52,7 @@ $().ready(() => {
   function mkModalBody(tittle) {
     $('.modal-content').empty().append(`
               <div class="modal-header">
-          <h5 class="modal-title" id="studentForm">${tittle}</h5>
+          <h5 class="modal-title" id="studentForm">${tittle} STUDENT</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
@@ -72,22 +73,24 @@ $().ready(() => {
             <input type="text" id="city" class="form-control" placeholder="City" required>
             <input type="text" id="postal" class="form-control" placeholder="Postal" required>
             <input type="text" id="subjects" class="form-control" placeholder="subjects" required>
+            <div class="form-group row">
+              <div class="col-sm-10 form-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-primary">Save</button>
+              </div>
+            </div>
           </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-          <button type="button" class="btn btn-primary">Save</button>
-        </div>
-      `);
+          <div class="modal-footer">
+          </div>
+        </div>`);
   }
 
-  function loadFreshData(dbServer) {
+  function loadFreshData() {
     $.ajax(`${server}/api/students`, {
         method: 'GET',
         contentType: 'application/json',
       })
       .done((data) => {
-        console.log(data);
         $('.table').empty().append(mkThead);
         data.forEach((student, index) => {
           $('.table').append(mkTbody(student, index));
@@ -99,24 +102,23 @@ $().ready(() => {
   }
 
   // delete student
-  function deleteStudent(id, dbServer) {
-    console.log('deleting', id);
-    $.ajax(`${dbServer}/api/student/${id}`, {
+  function deleteStudent(id) {
+    $.ajax(`${server}/api/student/${id}`, {
         method: 'DELETE',
         contentType: 'application/json',
       })
       .done((data) => {
+        loadFreshData(server);
         console.log(data.message);
-        loadFreshData(dbServer);
       })
       .fail((data) => {
         console.log('error from server');
       });
   }
 
-  function fillModalEdit(dbServer, studentId) {
-    $('.modal-footer button:nth-child(2)').attr('id', studentId).addClass('save');
-    $.ajax(`${dbServer}/api/student/${studentId}`, {
+  function fillModalEdit(studentId) {
+    $('.form-footer button:nth-child(2)').attr('id', studentId).addClass('save');
+    $.ajax(`${server}/api/student/${studentId}`, {
         method: 'GET',
         contentType: 'application/json',
       })
@@ -133,10 +135,10 @@ $().ready(() => {
       });
   }
 
-  function createEditStudent(method, dbServer, studentId) {
-    let ajaxCall = `${dbServer}/api/student/${studentId}`;
+  function createEditStudent(method, studentId) {
+    let ajaxCall = `${server}/api/student/${studentId}`;
     if (!studentId) {
-      ajaxCall = `${dbServer}/api/student`;
+      ajaxCall = `${server}/api/student`;
 
     }
     const name = $('#name').val();
@@ -146,33 +148,41 @@ $().ready(() => {
     const gender = $('.form-check-input').val();
     const postal = $('#postal').val();
     const subjects = $('#subjects').val().split(',');
-    $.ajax(ajaxCall, {
-        method: method,
-        contentType: 'application/json',
-        data: JSON.stringify({
-          name: name,
-          age: age,
-          gender: gender,
-          street: street,
-          postal: postal,
-          city: city,
-          subjects: subjects
-        }),
-      })
-      .done((data) => {
-        console.log(data.message);
-        $('.modal').modal('toggle');
-        loadFreshData(server);
-      })
-      .fail((data) => {
-        console.log(data.err);
-      });
+    //  control if the fields are empty
+    if ((name && age && street && city && postal && gender && subjects) === '') {
+      console.log('meeeekkk');
+      return $('.modal-footer').empty().append(`<div class="alert alert-danger" role="alert">
+                      All field must be filled!
+                    </div>`);
+    } else {
+      $.ajax(ajaxCall, {
+          method: method,
+          contentType: 'application/json',
+          data: JSON.stringify({
+            name: name,
+            age: age,
+            gender: gender,
+            street: street,
+            postal: postal,
+            city: city,
+            subjects: subjects
+          }),
+        })
+        .done((data) => {
+          console.log(data.message);
+          $('.modal').modal('toggle');
+          loadFreshData(server);
+        })
+        .fail((data) => {
+          console.log(data.err);
+        });
+    }
   }
 
   // search one student by id
-  function findStudent(dbServer) {
+  function findStudent() {
     const studentId = $('#studentId').val();
-    $.ajax(`${dbServer}/api/student/${studentId}`, {
+    $.ajax(`${server}/api/student/${studentId}`, {
         method: 'GET',
         contentType: 'application/json',
       })
@@ -197,21 +207,21 @@ $().ready(() => {
     if (clickId !== null) {
       const clickClass = target.getAttribute('class');
       if (clickClass.match('editbtn')) {
-        mkModalBody('EDIT STUDENT');
-        fillModalEdit(server, clickId);
+        mkModalBody('EDIT');
+        fillModalEdit(clickId);
       } else if (clickClass.match('deletebtn')) {
-        deleteStudent(clickId, server);
+        deleteStudent(clickId);
       } else if (clickId.match('createStudent')) {
-        mkModalBody('CREATE STUDENT');
-        $('.modal-footer button:nth-child(2)').attr('id','create');
+        mkModalBody('CREATE');
+        $('.form-footer button:nth-child(2)').attr('id', 'create');
       } else if (clickId.match('searchStudent')) {
-        findStudent(server);
+        findStudent();
       } else if (clickId.match('showStudents')) {
         loadFreshData(server);
       } else if (clickClass.match('save')) {
-        createEditStudent('PUT', server, clickId);
+        createEditStudent('PUT', clickId);
       } else if (clickId.match('create')) {
-        createEditStudent('POST', server);
+        createEditStudent('POST');
       }
     }
   });
